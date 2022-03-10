1ea5f8101c6701e43b44476efcda7930b4652226
BlueMir <bluemir@bluemir.me>
2022-03-10 23:10:37 +0900

    Make it thread safe

--- pkg/util/console/console.go
+++ pkg/util/console/console.go
@@ -4,13 +4,14 @@ import (
 	"bufio"
 	"bytes"
 	"io"
-	"sync/atomic"
+	"sync"
 )
 
 // writer but has little tweak each line
 type Console struct {
-	output     io.Writer
-	firstWrite *int32 // TODO make atomic
+	output  io.Writer
+	written *bool
+	mutex   *sync.Mutex
 
 	buf     *bytes.Buffer
 	scanner *bufio.Scanner
@@ -25,21 +26,21 @@ func New(output io.Writer) *Console {
 
 	scanner := bufio.NewScanner(buf)
 
-	firstWrite := int32(0)
+	written := false
 
 	return &Console{
-		output:     output,
-		firstWrite: &firstWrite,
-		buf:        buf,
-		scanner:    scanner,
-		modifier:   nil,
+		output:   output,
+		written:  &written,
+		mutex:    &sync.Mutex{},
+		buf:      buf,
+		scanner:  scanner,
+		modifier: nil,
 	}
 }
 
-func (c *Console) Write(p []byte) (n int, err error) {
-	n, err = c.buf.Write(p)
-	if err != nil {
-		return
+func (c *Console) Write(p []byte) (int, error) {
+	if n, err := c.buf.Write(p); err != nil {
+		return n, err
 	}
 
 	for c.scanner.Scan() {
@@ -48,21 +49,25 @@ func (c *Console) Write(p []byte) (n int, err error) {
 			str = m(str)
 		}
 
-		// QUESTION is it thread-safe?
-		if !atomic.CompareAndSwapInt32(c.firstWrite, 0, 1) {
-			_, err = c.output.Write(append([]byte{'\n'}, []byte(str)...))
-			if err != nil {
-				return n, err
-			}
-		} else {
-			_, err = c.output.Write([]byte(str))
-			if err != nil {
-				return n, err
-			}
+		if _, err := c.writeWithMutex([]byte(str)); err != nil {
+			return len(p), err
 		}
 	}
+	return len(p), nil
+}
+func (c *Console) writeWithMutex(p []byte) (int, error) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
 
-	return
+	if *c.written {
+		if _, err := c.output.Write([]byte{'\n'}); err != nil {
+			return 0, err
+		}
+	} else {
+		*c.written = true
+	}
+
+	return c.output.Write(p)
 }
 func (c *Console) Close() error {
 	// flush
@@ -83,10 +88,11 @@ func (c *Console) WithModifier(m Modifier) *Console {
 	scanner := bufio.NewScanner(buf)
 
 	return &Console{
-		output:     c.output,
-		firstWrite: c.firstWrite,
-		buf:        buf,
-		scanner:    scanner,
-		modifier:   append([]Modifier{m}, c.modifier...),
+		output:   c.output,
+		written:  c.written,
+		mutex:    c.mutex,
+		buf:      buf,
+		scanner:  scanner,
+		modifier: append([]Modifier{m}, c.modifier...),
 	}
 }
